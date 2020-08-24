4a6a28c95921c7b2c3744e674ee5f0707ac79f30
ej550 <edward.oughton@gmail.com>
2020-08-24 10:23:43 +0100

    Update tests

---
+++ tests/test_assess.py
@@ -0,0 +1,265 @@
+import pytest
+from src.india5g.assess import (get_subscriber_aquisition_cost, get_spectrum_costs, calculate_tax,
+    calculate_profit, calculate_benefit_cost_ratio, assess, estimate_subsidies,
+    allocate_available_excess)
+
+
+def test_get_subscriber_aquisition_cost(setup_region, setup_country_parameters):
+
+    setup_region[0]['phones_on_network'] = 10
+
+    answer = get_subscriber_aquisition_cost(setup_region[0], setup_country_parameters)
+
+    answer['ops_and_acquisition'] = (
+        setup_region[0]['phones_on_network'] * setup_country_parameters['financials']['ops_and_acquisition_per_subscriber'])
+
+    # answer['ops_and_admin'] = (
+    #     setup_region[0]['phones_on_network'] * setup_country_parameters['financials']['ops_and_admin_per_subscriber'])
+
+
+def test_get_spectrum_costs(setup_region, setup_option, setup_global_parameters, setup_country_parameters):
+
+    setup_region[0]['new_sites'] = 1
+
+    # 10000 people
+    # 200000 = 1 * 20 * 10000 (cost = cost_mhz_pop * bw * pop )
+    # 200000 = 1 * 20 * 10000 (cost = cost_mhz_pop * bw * pop )
+    assert get_spectrum_costs(setup_region[0], setup_option['strategy'],
+        setup_global_parameters, setup_country_parameters) == 400000
+
+    setup_region[0]['new_sites'] = 1
+
+    # test high spectrum costs which are 50% higher
+    assert get_spectrum_costs(setup_region[0], '4G_epc_microwave_baseline_baseline_high_baseline',
+        setup_global_parameters, setup_country_parameters) == (
+            400000 * (setup_country_parameters['financials']['spectrum_cost_high'] / 100))
+
+    # test low spectrum costs which are 50% lower
+    assert get_spectrum_costs(setup_region[0], '4G_epc_microwave_baseline_baseline_low_baseline',
+        setup_global_parameters, setup_country_parameters) == (
+            400000 * (setup_country_parameters['financials']['spectrum_cost_low'] / 100))
+
+
+def test_calculate_tax(setup_region, setup_option, setup_country_parameters):
+
+    setup_region[0]['total_revenue'] = 1e7
+    setup_region[0]['network_cost'] = 1e6
+    setup_region[0]['spectrum_cost'] = 1e1
+
+    assert calculate_tax(setup_region[0], setup_option['strategy'], setup_country_parameters) == 1e6 * (25/100)
+
+    setup_region[0]['network_cost'] = 1e6
+    setup_option['strategy'] = '4G_epc_microwave_baseline_baseline_baseline_high'
+
+    answer = calculate_tax(setup_region[0], setup_option['strategy'], setup_country_parameters)
+
+    assert answer == 1e6 * (40/100)
+
+    setup_region[0]['network_cost'] = 1e6
+    setup_option['strategy'] = '4G_epc_microwave_baseline_baseline_baseline_low'
+
+    answer = calculate_tax(setup_region[0], setup_option['strategy'], setup_country_parameters)
+
+    assert answer == 1e6 * (10/100)
+
+    setup_region[0]['total_revenue'] = 1e7
+    setup_region[0]['network_cost'] = 1e9
+    setup_region[0]['spectrum_cost'] = 1e1
+
+    assert calculate_tax(setup_region[0], setup_option['strategy'], setup_country_parameters) == 1e8
+
+
+def test_calculate_profit(setup_region, setup_country_parameters):
+
+    setup_region[0]['network_cost'] = 1e6
+    setup_region[0]['spectrum_cost'] = 6e4
+    setup_region[0]['tax'] = 265e3
+
+    assert calculate_profit(setup_region[0], setup_country_parameters) == 265e3
+
+
+def test_calculate_benefit_cost_ratio(setup_region, setup_country_parameters):
+
+    setup_region[0]['network_cost'] = 1e6
+    setup_region[0]['spectrum_cost'] = 6e4
+    setup_region[0]['tax'] = 265e3
+    setup_region[0]['profit_margin'] = 265e3
+    setup_region[0]['total_revenue'] = 159e4
+    setup_region[0]['used_cross_subsidy'] = 0
+
+    assert calculate_benefit_cost_ratio(setup_region[0], setup_country_parameters) == 1
+
+    setup_region[0]['used_cross_subsidy'] = 159e4
+
+    assert calculate_benefit_cost_ratio(setup_region[0], setup_country_parameters) == 1
+
+
+def test_estimate_subsidies():
+
+    region = {
+            'GID_id': 'a',
+            'total_revenue': 10000,
+            'total_cost': 5000,
+            'available_cross_subsidy': 5000,
+            'deficit': 0,
+        }
+
+    answer, available_cross_subsidy = estimate_subsidies(region, 0)
+
+    assert answer['available_cross_subsidy'] == 5000
+    assert answer['used_cross_subsidy'] == 0
+    assert answer['required_state_subsidy'] == 0
+    assert available_cross_subsidy == 0
+
+    region = {
+            'GID_id': 'a',
+            'total_revenue': 5000,
+            'total_cost': 10000,
+            'available_cross_subsidy': 0,
+            'deficit': 5000,
+        }
+
+    answer, available_cross_subsidy = estimate_subsidies(region, 5000)
+
+    assert answer['available_cross_subsidy'] == 0
+    assert answer['used_cross_subsidy'] == 5000
+    assert answer['required_state_subsidy'] == 0
+    assert available_cross_subsidy == 0
+
+    region = {
+            'GID_id': 'a',
+            'total_revenue': 5000,
+            'total_cost': 10000,
+            'available_cross_subsidy': 0,
+            'deficit': 5000,
+        }
+
+    answer, available_cross_subsidy = estimate_subsidies(region, 0)
+
+    assert answer['available_cross_subsidy'] == 0
+    assert answer['used_cross_subsidy'] == 0
+    assert answer['required_state_subsidy'] == 5000
+    assert available_cross_subsidy == 0
+
+    region = {
+            'GID_id': 'a',
+            'total_revenue': 5000,
+            'total_cost': 10000,
+            'available_cross_subsidy': 0,
+            'deficit': 5000,
+        }
+
+    answer, available_cross_subsidy = estimate_subsidies(region, 2500)
+
+    assert answer['available_cross_subsidy'] == 0
+    assert answer['used_cross_subsidy'] == 2500
+    assert answer['required_state_subsidy'] == 2500
+    assert available_cross_subsidy == 0
+
+
+def test_assess(setup_option, setup_global_parameters, setup_country_parameters, setup_costs):
+
+    regions = [
+        {
+            'GID_id': 'a',
+            'population': 1000,
+            'population_km2': 500,
+            'total_revenue': 20000,
+            'network_cost': 5000,
+            'smartphones_on_network': 250,
+            'phones_on_network': 500,
+        },
+        {
+            'GID_id': 'b',
+            'population': 500,
+            'population_km2': 250,
+            'total_revenue': 12000,
+            'network_cost': 8000,
+            'smartphones_on_network': 250,
+            'phones_on_network': 500,
+        },
+    ]
+
+    setup_country_parameters['financials']['spectrum_coverage_baseline_usd_mhz_pop'] = 0.125
+    setup_country_parameters['financials']['spectrum_capacity_baseline_usd_mhz_pop'] = 0.025
+
+    answer = assess('MWI', regions, setup_option, setup_global_parameters,
+        setup_country_parameters, setup_costs)
+
+    assert answer[0]['total_revenue'] == 20000
+    assert answer[0]['network_cost'] == 5000
+    assert answer[0]['ops_and_acquisition'] == 500
+    assert answer[0]['ops_and_acquisition'] == 500
+    assert answer[0]['spectrum_cost'] == 3000
+    assert answer[0]['tax'] == 1250
+    assert answer[0]['profit_margin'] == 1850
+    assert answer[0]['total_cost'] == 11600.0
+    assert answer[0]['available_cross_subsidy'] == 8400.0
+    assert answer[0]['used_cross_subsidy'] == 0
+    assert answer[0]['bcr'] == 1.8018018018018018
+    assert answer[0]['required_state_subsidy'] == 0
+
+    assert answer[1]['total_revenue'] == 12000
+    assert answer[1]['network_cost'] == 8000
+    assert answer[1]['spectrum_cost'] == 1500
+    assert answer[1]['tax'] == 2000
+    assert answer[1]['profit_margin'] == 2300
+    assert answer[1]['total_cost'] == 14300
+    assert answer[1]['available_cross_subsidy'] == 0
+    assert answer[1]['used_cross_subsidy'] == 2300.0
+    assert answer[1]['bcr'] == 0.8695652173913043
+    assert answer[1]['required_state_subsidy'] == 0
+
+    regions = [
+        {
+            'GID_id': 'a',
+            'population': 1000,
+            'population_km2': 500,
+            'total_revenue': 20000,
+            'network_cost': 5200,
+            'smartphones_on_network': 250,
+            'phones_on_network': 500,
+        },
+        {
+            'GID_id': 'b',
+            'population': 1000,
+            'population_km2': 500,
+            'total_revenue': 2500,
+            'network_cost': 5200,
+            'smartphones_on_network': 250,
+            'phones_on_network': 500,
+        },
+    ]
+
+    answer = assess('MWI', regions, setup_option, setup_global_parameters,
+        setup_country_parameters, setup_costs)
+
+    assert answer[0]['available_cross_subsidy'] == 8100
+    assert answer[0]['used_cross_subsidy'] == 0
+    assert answer[0]['required_state_subsidy'] == 0
+    assert answer[1]['available_cross_subsidy'] == 0
+    assert answer[1]['used_cross_subsidy'] == 8100
+    assert answer[1]['required_state_subsidy'] == 1300
+
+
+def test_allocate_available_excess():
+
+    region = {
+            'total_revenue': 10000,
+            'total_cost': 5000,
+        }
+
+    answer = allocate_available_excess(region)
+
+    assert answer['available_cross_subsidy'] == 5000
+    assert answer['deficit'] == 0
+
+    regions = {
+            'total_revenue': 5000,
+            'total_cost': 10000,
+        }
+
+    answer = allocate_available_excess(regions)
+
+    assert answer['available_cross_subsidy'] == 0
+    assert answer['deficit'] == 5000
--- vis/panel_plots.r
+++ vis/panel_plots.r
@@ -5,114 +5,114 @@ library(ggpubr)
 
 ####################SUPPLY-DEMAND METRICS
 folder <- dirname(rstudioapi::getSourceEditorContext()$path)
-
-data <- read.csv(file.path(folder, '..', 'results', 'tc_decile_results_technology_options.csv'))
-
-data <- data[!(data$total_cost == "NA"),]
-
-names(data)[names(data) == 'GID_0'] <- 'country'
-
-#select desired columns
-data <- select(data, tc_code, scenario, strategy, confidence, decile, #population, area_km2, 
-               population_km2, sites_estimated_total, existing_network_sites,
-               sites_estimated_total_km2, existing_network_sites_km2,
-               phone_density_on_network_km2, sp_density_on_network_km2, 
-               total_revenue, total_cost, cost_per_network_user, 
-               # cost_per_sp_user
-               )
-
-data <- data[(data$confidence == 50),]
-
-data$combined <- paste(data$tc_code, data$scenario, sep="_")
-
-# data$tc_code = factor(data$tc_code, 
-#                       levels=c('IND'), 
-#                       labels=c("India"))
-
-demand = data[(
-  data$scenario == 'S1_25_10_2' &
-  data$strategy == '4G_epc_fiber_baseline_baseline_baseline_baseline'
-    ),]
-
-demand <- select(demand, tc_code, decile, population_km2, 
-                 phone_density_on_network_km2, sp_density_on_network_km2)
-
-demand <- gather(demand, metric, value, population_km2:sp_density_on_network_km2)
-
-demand$metric = factor(demand$metric, 
-                     levels=c("population_km2",
-                             "phone_density_on_network_km2",
-                             "sp_density_on_network_km2"),
-                       labels=c("Population Density",
-                                "Phone Density",
-                                "Smartphone Density"))
-
-demand <- demand[complete.cases(demand),]
-
-demand_densities <- ggplot(demand, aes(x=decile, y=value, colour=metric, group=metric)) + 
-  geom_line() +
-  scale_fill_brewer(palette="Spectral", name = expression('Cost Type'), direction=1) +
-  theme( legend.position = "bottom") + #axis.text.x = element_text(angle = 45, hjust = 1), 
-  labs(colour=NULL,
-       title = "Demand-Side Density Metrics by Population Decile",
-       # subtitle = "Population and user densities",
-       x = "Population Decile", y = "Density (per km^2)") + 
-  scale_x_continuous(expand = c(0, 0), breaks = seq(0,100,20)) + 
-  scale_y_continuous(expand = c(0, 0)) + #, limits = c(0,20)) +  
-  theme(panel.spacing = unit(0.6, "lines")) + expand_limits(y=0) +
-  guides(colour=guide_legend(ncol=3)) +
-  facet_wrap(~tc_code, scales = "free", ncol=5) 
-
-#export to folder
-path = file.path(folder, 'figures', 'demand_panel.png')
-ggsave(path,  units="in", width=8, height=9, dpi=300)
-print(demand_densities)
-dev.off()
-
-supply = data[(
-  data$scenario == 'S1_25_10_2' &
-    data$strategy == '4G_epc_fiber_baseline_baseline_baseline_baseline'
-),]
-
-supply <- select(supply, tc_code, decile, sites_estimated_total_km2, existing_network_sites_km2)
-
-supply <- gather(supply, metric, value, sites_estimated_total_km2:existing_network_sites_km2)
-
-supply$metric = factor(supply$metric, 
-                       levels=c("sites_estimated_total_km2",
-                                "existing_network_sites_km2"),
-                       labels=c("Total Site Density",
-                                "Modeled Network Site Density"))
-
-supply <- supply[complete.cases(supply),]
-
-supply_densities <- ggplot(supply, aes(x=decile, y=value, colour=metric, group=metric)) + 
-  geom_line() +
-  scale_fill_brewer(palette="Spectral", name = expression('Cost Type'), direction=1) +
-  theme( legend.position = "bottom") + #axis.text.x = element_text(angle = 45, hjust = 1), 
-  labs(colour=NULL,
-       title = "Supply-Side Density Metrics by Population Decile",
-       # subtitle = "Cumulative cost reported by percentage of population covered",
-       x = "Population decile", y = "Density (per km^2)") + 
-  scale_x_continuous(expand = c(0, 0), breaks = seq(0,100,20)) + 
-  scale_y_continuous(expand = c(0, 0)) + #, limits = c(0,20)) +  
-  theme(panel.spacing = unit(0.6, "lines")) + expand_limits(y=0) +
-  guides(colour=guide_legend(ncol=3)) +
-  facet_wrap(~tc_code, scales = "free", ncol=5) 
-
-#export to folder
-path = file.path(folder, 'figures', 'supply_panel.png')
-ggsave(path,  units="in", width=8, height=9, dpi=300)
-print(supply_densities)
-dev.off()
-
-demand_supply <- ggarrange(demand_densities, supply_densities, ncol = 1, nrow = 2, align = c("hv"))
-
-#export to folder
-path = file.path(folder, 'figures', 'a_demand_supply_panel.png')
-ggsave(path,  units="in", width=8, height=9, dpi=300)
-print(demand_supply)
-dev.off()
+# 
+# data <- read.csv(file.path(folder, '..', 'results', 'tc_decile_results_technology_options.csv'))
+# 
+# data <- data[!(data$total_cost == "NA"),]
+# 
+# names(data)[names(data) == 'GID_0'] <- 'country'
+# 
+# #select desired columns
+# data <- select(data, tc_code, scenario, strategy, confidence, decile, #population, area_km2, 
+#                population_km2, sites_estimated_total, existing_network_sites,
+#                sites_estimated_total_km2, existing_network_sites_km2,
+#                phone_density_on_network_km2, sp_density_on_network_km2, 
+#                total_revenue, total_cost, cost_per_network_user, 
+#                # cost_per_sp_user
+#                )
+# 
+# data <- data[(data$confidence == 50),]
+# 
+# data$combined <- paste(data$tc_code, data$scenario, sep="_")
+# 
+# # data$tc_code = factor(data$tc_code, 
+# #                       levels=c('IND'), 
+# #                       labels=c("India"))
+# 
+# demand = data[(
+#   data$scenario == 'S1_25_10_2' &
+#   data$strategy == '4G_epc_fiber_baseline_baseline_baseline_baseline'
+#     ),]
+# 
+# demand <- select(demand, tc_code, decile, population_km2, 
+#                  phone_density_on_network_km2, sp_density_on_network_km2)
+# 
+# demand <- gather(demand, metric, value, population_km2:sp_density_on_network_km2)
+# 
+# demand$metric = factor(demand$metric, 
+#                      levels=c("population_km2",
+#                              "phone_density_on_network_km2",
+#                              "sp_density_on_network_km2"),
+#                        labels=c("Population Density",
+#                                 "Phone Density",
+#                                 "Smartphone Density"))
+# 
+# demand <- demand[complete.cases(demand),]
+# 
+# demand_densities <- ggplot(demand, aes(x=decile, y=value, colour=metric, group=metric)) + 
+#   geom_line() +
+#   scale_fill_brewer(palette="Spectral", name = expression('Cost Type'), direction=1) +
+#   theme( legend.position = "bottom") + #axis.text.x = element_text(angle = 45, hjust = 1), 
+#   labs(colour=NULL,
+#        title = "Demand-Side Density Metrics by Population Decile",
+#        # subtitle = "Population and user densities",
+#        x = "Population Decile", y = "Density (per km^2)") + 
+#   scale_x_continuous(expand = c(0, 0), breaks = seq(0,100,20)) + 
+#   scale_y_continuous(expand = c(0, 0)) + #, limits = c(0,20)) +  
+#   theme(panel.spacing = unit(0.6, "lines")) + expand_limits(y=0) +
+#   guides(colour=guide_legend(ncol=3)) +
+#   facet_wrap(~tc_code, scales = "free", ncol=5) 
+# 
+# #export to folder
+# path = file.path(folder, 'figures', 'demand_panel.png')
+# ggsave(path,  units="in", width=8, height=9, dpi=300)
+# print(demand_densities)
+# dev.off()
+# 
+# supply = data[(
+#   data$scenario == 'S1_25_10_2' &
+#     data$strategy == '4G_epc_fiber_baseline_baseline_baseline_baseline'
+# ),]
+# 
+# supply <- select(supply, tc_code, decile, sites_estimated_total_km2, existing_network_sites_km2)
+# 
+# supply <- gather(supply, metric, value, sites_estimated_total_km2:existing_network_sites_km2)
+# 
+# supply$metric = factor(supply$metric, 
+#                        levels=c("sites_estimated_total_km2",
+#                                 "existing_network_sites_km2"),
+#                        labels=c("Total Site Density",
+#                                 "Modeled Network Site Density"))
+# 
+# supply <- supply[complete.cases(supply),]
+# 
+# supply_densities <- ggplot(supply, aes(x=decile, y=value, colour=metric, group=metric)) + 
+#   geom_line() +
+#   scale_fill_brewer(palette="Spectral", name = expression('Cost Type'), direction=1) +
+#   theme( legend.position = "bottom") + #axis.text.x = element_text(angle = 45, hjust = 1), 
+#   labs(colour=NULL,
+#        title = "Supply-Side Density Metrics by Population Decile",
+#        # subtitle = "Cumulative cost reported by percentage of population covered",
+#        x = "Population decile", y = "Density (per km^2)") + 
+#   scale_x_continuous(expand = c(0, 0), breaks = seq(0,100,20)) + 
+#   scale_y_continuous(expand = c(0, 0)) + #, limits = c(0,20)) +  
+#   theme(panel.spacing = unit(0.6, "lines")) + expand_limits(y=0) +
+#   guides(colour=guide_legend(ncol=3)) +
+#   facet_wrap(~tc_code, scales = "free", ncol=5) 
+# 
+# #export to folder
+# path = file.path(folder, 'figures', 'supply_panel.png')
+# ggsave(path,  units="in", width=8, height=9, dpi=300)
+# print(supply_densities)
+# dev.off()
+# 
+# demand_supply <- ggarrange(demand_densities, supply_densities, ncol = 1, nrow = 2, align = c("hv"))
+# 
+# #export to folder
+# path = file.path(folder, 'figures', 'a_demand_supply_panel.png')
+# ggsave(path,  units="in", width=8, height=9, dpi=300)
+# print(demand_supply)
+# dev.off()
 
 ####################TECHNOLOGIES BY DECILE
 folder <- dirname(rstudioapi::getSourceEditorContext()$path)
@@ -166,18 +166,58 @@ technology_costs <- ggplot(wide, aes(x=decile, y=mean/1e9, fill=asset)) +
   labs(colour=NULL,
        title = "Cost performance of 5G infrastructure Strategies for India",
        subtitle = "Results reported by scenario, strategy and population decile at 95% confidence",
-       x = NULL, y = "Investment Cost ($USD Billions)") +
+       x = 'Population Decile', y = "Investment Cost ($USD Billions)", fill='Cost Type') +
   theme(panel.spacing = unit(0.6, "lines"), axis.text.x = element_text(angle = 45)) + 
   expand_limits(y=0) +
   guides() +
   facet_grid(strategy~scenario)
 
 path = file.path(folder, 'figures', 'technology_options.png')
-ggsave(path, units="in", width=10, height=14.5, dpi=300)
+ggsave(path, units="in", width=10, height=10, dpi=300)
 print(technology_costs)
 dev.off()
 
 
+####################TECHNOLOGIES BY DECILE
+folder <- dirname(rstudioapi::getSourceEditorContext()$path)
+
+data <- read.csv(file.path(folder, '..', 'results', 'decile_results_technology_options.csv'))
+
+data <- data[!(data$total_cost == "NA"),]
+
+data <- data[(data$confidence == 50),]
+
+#select desired columns
+data <- select(data, scenario, strategy, decile, area_km2, population, total_cost, total_revenue)
+
+data$scenario = factor(data$scenario, levels=c("S1_25_10_2",
+                                               "S2_200_50_5",
+                                               "S3_400_100_10"),
+                       labels=c("S1 (25 Mbps)",
+                                "S2 (200 Mbps)",
+                                "S3 (400 Mbps)"))
+
+data <- data[order(data$scenario, data$strategy, data$decile),]
+
+data1 <- select(data, scenario, strategy, decile, total_revenue)
+data1 <- data1[(data1$strategy == "4G_epc_microwave_baseline_baseline_baseline_baseline"),]
+data1$strategy <- "Revenue" 
+names(data1)[names(data1) == 'total_revenue'] <- 'value'
+data2 <- select(data, scenario, strategy, decile, total_cost)
+names(data2)[names(data2) == 'total_cost'] <- 'value'
+data <- rbind(data1, data2)
+remove(data1, data2)
+
+data$strategy = factor(data$strategy, levels=c("Revenue",
+                                               "4G_epc_microwave_baseline_baseline_baseline_baseline",
+                                               "4G_epc_fiber_baseline_baseline_baseline_baseline",
+                                               "5G_nsa_microwave_baseline_baseline_baseline_baseline",
+                                               "5G_sa_fiber_baseline_baseline_baseline_baseline"),
+                       labels=c("Revenue",
+                                "4G (Wireless)",
+                                "4G (Fiber)",
+                                "5G NSA (Wireless)",
+                                "5G SA (Fiber)"))
 
 data <- data[order(data$scenario, data$strategy, data$decile),]
 
@@ -185,7 +225,7 @@ data <- data %>%
   group_by(scenario, strategy) %>%
   mutate(cumulative_value_bn = cumsum(round(value / 1e9, 3)))
 
-ggplot(data, aes(x=decile, y=cumulative_value_bn)) + #, colour=strategy, group=strategy
+panel <- ggplot(data, aes(x=decile, y=cumulative_value_bn, colour=strategy, group=strategy)) + 
   geom_line() +
   scale_fill_brewer(palette="Spectral", name = expression('Cost Type'), direction=1) +
   theme( legend.position = "bottom") + #axis.text.x = element_text(angle = 45, hjust = 1), 
@@ -197,12 +237,25 @@ ggplot(data, aes(x=decile, y=cumulative_value_bn)) + #, colour=strategy, group=s
   scale_y_continuous(expand = c(0, 0)) + #, limits = c(0,20)) +  
   theme(panel.spacing = unit(0.6, "lines")) + expand_limits(y=0) +
   guides(colour=guide_legend(ncol=5)) +
-  facet_grid(scenario~strategy) 
+  facet_wrap(~scenario) 
 
 path = file.path(folder, 'figures', 'b_results_technology_options_wrap.png')
 ggsave(path, units="in", width=10, height=14.5, dpi=300)
 print(panel)
 dev.off()
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 ##################BUSINESS MODELS BY DECILE
 #load data
